587e05a1e4e1dad9ba8d40e89ca1e3f07789e166
Sérgio Miguel Fernandes <sergio.fernandes@ist.utl.pt>
2013-03-01 11:07:18 +0000

    Add test folder with a simple test
      * The default factory returns an advice that runs the same advised method
        twice.

---
+++ advice/src/test/java/pt/ist/esw/advice/clientimpl/DefaultContextFactory.java
@@ -0,0 +1,49 @@
+/*
+ * AtomicAnnotation
+ * Copyright (C) 2012 INESC-ID Software Engineering Group
+ * http://www.esw.inesc-id.pt
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author's contact:
+ * INESC-ID Software Engineering Group
+ * Rua Alves Redol 9
+ * 1000 - 029 Lisboa
+ * Portugal
+ */
+package pt.ist.esw.advice.clientimpl;
+
+import java.util.concurrent.Callable;
+
+import pt.ist.esw.advice.Advice;
+import pt.ist.esw.advice.AdviceFactory;
+import pt.ist.esw.advice.Atomic;
+
+public class DefaultContextFactory extends AdviceFactory {
+
+    public static class MyAdvice implements Advice {
+
+        public <V> V perform(Callable<V> method) throws Exception {
+            method.call();
+            method.call();
+            return null;
+        }
+    }
+
+    public static Advice newContext(Atomic atomic) {
+        return new MyAdvice();
+    }
+
+}
